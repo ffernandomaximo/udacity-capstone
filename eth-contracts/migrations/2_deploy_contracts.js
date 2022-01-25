a957fa74e4cb93a3ebd6dca09f5cf9bcecd226d8
Fernando Maximo Ferreira <fernandomaximof@Fernandos-MBP.hub>
2022-01-25 22:31:19 +1300

    deployed contract

--- eth-contracts/migrations/2_deploy_contracts.js
+++ eth-contracts/migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 // migrating the appropriate contracts
-var Verifier = artifacts.require("./Verifier.sol");
+var Verifier = artifacts.require("./verifier.sol");
 var SolnSquareVerifier = artifacts.require("./SolnSquareVerifier.sol");
 
 module.exports = async function (deployer) {
